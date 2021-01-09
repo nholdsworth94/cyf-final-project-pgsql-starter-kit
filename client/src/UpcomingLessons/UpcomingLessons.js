e59462b505b3a2223ab84f93d09f753db268e797
Ali Haider <alihaiderx736@gmail.com>
2021-01-09 02:08:49 +0000

    - test heroku fetch

--- client/src/UpcomingLessons/UpcomingLessons.js
+++ client/src/UpcomingLessons/UpcomingLessons.js
@@ -16,7 +16,7 @@ const UpcomingLessons =({RolesButtonHandler})=>{
 
 	const fetchData = async () => {
 		try {
-			const response = await fetch('/lesson');
+			const response = await fetch('https://cyf-finalproject-class-planner.herokuapp.com/api/school');
 			const data = await response.json();
 			console.log(data.data);
 			setLessons(data.data);
