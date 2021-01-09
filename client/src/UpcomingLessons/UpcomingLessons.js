d1b208dc2c133f97e2f137ea7539c366a614b84a
Ali Haider <alihaiderx736@gmail.com>
2021-01-09 01:58:41 +0000

    - heroku fetch test

--- client/src/UpcomingLessons/UpcomingLessons.js
+++ client/src/UpcomingLessons/UpcomingLessons.js
@@ -16,7 +16,7 @@ const UpcomingLessons =({RolesButtonHandler})=>{
 
 	const fetchData = async () => {
 		try {
-			const response = await fetch('/api/lesson');
+			const response = await fetch('/lesson');
 			const data = await response.json();
 			console.log(data.data);
 			setLessons(data.data);
