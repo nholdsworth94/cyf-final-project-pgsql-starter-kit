246205b140c16299dda085118477b160d5af0af1
Ali Haider <alihaiderx736@gmail.com>
2021-01-10 22:25:18 +0000

    - update heroku test

--- client/src/RegisterPage/RegisterPage.js
+++ client/src/RegisterPage/RegisterPage.js
@@ -1,7 +1,9 @@
 import React from "react";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
+
 const RegisterPage = () => {
+
   return (
       <div>
       <Header/> 
@@ -11,12 +13,14 @@ const RegisterPage = () => {
   </div>
 </div>
 <div className="form">
-  <form className="login-form">
-    <input type="text" placeholder="first name"/>
-    <input type="text" placeholder="last name"/>
-    <input type="text" placeholder="email"/>
-    <input type="password" placeholder="password"/>
-    <button>Register</button>
+  <form className="login-form" method="POST" action="https://cyf-finalproject-class-planner.herokuapp.com/api/register">
+   <input type="text" placeholder="first name" name="fname"/>
+    <input type="text" placeholder="last name" name="lname"/>
+    <input type="text" placeholder="email" name="email"/>
+    <input type="password" placeholder="password" name="password"/>
+
+    <button type="submit" value="submit">Register</button>
+
     <p className="message">Already on CodeYourFuture? <a href="#">Log In</a></p>
   </form>
 </div>
