7296f0ba11305cf972eb810a3083ecacd26d44b7
Ali Haider <alihaiderx736@gmail.com>
2021-01-11 21:17:47 +0000

    - Import useLocation

--- client/src/LessonPage/LessonPage.js
+++ client/src/LessonPage/LessonPage.js
@@ -8,13 +8,22 @@ import LessonPage_Info from "./LessonPage_Info";
 import LessonPage_Volunteers from "./LessonPage_Volunteers";
 import Footer from "../Footer/Footer";
 import AvailableRoles from "./AvailableRoles";
+import {  useLocation } from "react-router-dom";
+ 
 
 
-const LessonPage = ({lessonId}) => {
+const LessonPage = () => {
+
+    let lessonId= useLocation().state.id;
+    let lesson= useLocation().state.lesson;
+    let module= useLocation().state.module;
+    let time= useLocation().state.time;
+    let date = useLocation().state.date;
+    let link=useLocation().state.link;
     return (
         <div className="App">
             <Header />
-            <LessonPage_Info />
+            <LessonPage_Info lessonId={lessonId} lesson={lesson} module={module} time={time} date={date} link={link}/>
             <AvailableRoles/>
             <LessonPage_Volunteers />
             <Footer />
