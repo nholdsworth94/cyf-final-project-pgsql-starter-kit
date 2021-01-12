a432a2d8560821feed4340e4d77831d594b573b4
Ali Haider <alihaiderx736@gmail.com>
2021-01-12 17:02:29 +0000

    - update available roles

--- client/src/LessonPage/AvailableRoles.js
+++ client/src/LessonPage/AvailableRoles.js
@@ -4,7 +4,7 @@ const AvailableRoles = () =>{
     return (
     <div className="row-10">
         <h4 className="container col-8 my-2"><strong>Available Roles:</strong></h4>
-            <form class=" container col-8">
+            <form class=" container col-8" method="POST" action="https://cyf-finalproject-class-planner.herokuapp.com/api/signed_volunteers">
 <div className="row">
       <div className="col mx-4">
                 <div class="form-check ">
@@ -38,14 +38,14 @@ const AvailableRoles = () =>{
 </div>
 <div className="form-inline mt-2">
                 <div class="form-group mx-2 mb-2 ">
-    <label for="inputPassword2" class="sr-only">Full Name</label>
-    <input type="text" class="form-control" id="inputPassword2" placeholder="Full Name"/>
+    <label for="inputPassword2" class="sr-only" >Full Name</label>
+    <input type="text" class="form-control" placeholder="Full Name" name="fullName"/>
   </div>
   <div class="form-group mx-2 mb-2">
     <label for="staticEmail2" class="sr-only">Email</label>
-    <input type="text" class="form-control" id="staticEmail2" placeholder="Email"/>
+    <input type="text" class="form-control" placeholder="Email" name="password"/>
   </div>
-  <button type="submit" class="btn btn-danger mx-2 mb-2">Class Sign Up</button>
+  <button type="submit" value="submit" class="btn btn-danger mx-2 mb-2">Class Sign Up</button>
   </div>
 </form>
         </div>
