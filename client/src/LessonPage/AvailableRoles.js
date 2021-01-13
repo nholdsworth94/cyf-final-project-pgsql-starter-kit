c4cafb6d2f6b564d101c4e5d0665a12d4db03072
Ali Haider <alihaiderx736@gmail.com>
2021-01-13 01:28:52 +0000

    - Update Available Roles

--- client/src/LessonPage/AvailableRoles.js
+++ client/src/LessonPage/AvailableRoles.js
@@ -8,30 +8,30 @@ const AvailableRoles = ({lessonId}) =>{
 <div className="row">
       <div className="col mx-4">
                 <div class="form-check ">
-  <input class="form-check-input position-static" type="radio" name="role" value="Lead Teacher"/>
+  <input class="form-check-input position-static" type="radio" name="role" value="Lead Teacher" required/>
    <label class="form-check-label" for="inlineRadio1">Lead Teacher</label>
 </div>
       <div class="form-check">
-  <input class="form-check-input position-static" type="radio" name="role" value="Assistant Lead Teacher"/>
+  <input class="form-check-input position-static" type="radio" name="role" value="Assistant Lead Teacher" required/>
   <label class="form-check-label" for="inlineRadio1">Assistant Lead Teacher</label>
 </div>
       <div class="form-check">
-  <input class="form-check-input position-static" type="radio" name="role" value="Teaching Assistant"/>
+  <input class="form-check-input position-static" type="radio" name="role" value="Teaching Assistant" required/>
   <label class="form-check-label" for="inlineRadio1">Teaching Assistant</label>
 </div>
      
 </div>
  <div className="col mx-4">
     <div class="form-check">
-  <input class="form-check-input position-static" type="radio" name="role" value="Coordinator"/>
+  <input class="form-check-input position-static" type="radio" name="role" value="Coordinator" required/>
   <label class="form-check-label" for="inlineRadio1">Coordinator</label>
 </div>
    <div class="form-check ">
-  <input class="form-check-input position-static" type="radio" name="role" value="Zoom Master (Host)"/>
+  <input class="form-check-input position-static" type="radio" name="role" value="Zoom Master (Host)" required/>
    <label class="form-check-label" for="inlineRadio1">Zoom Master (Host)</label>
 </div>
       <div class="form-check">
-  <input class="form-check-input position-static" type="radio" name="role" value="Personal Development Rep"/>
+  <input class="form-check-input position-static" type="radio" name="role" value="Personal Development Rep" required/>
   <label class="form-check-label" for="inlineRadio1">Personal Development Rep</label>
 </div>    
 </div>
@@ -39,11 +39,11 @@ const AvailableRoles = ({lessonId}) =>{
 <div className="form-inline mt-2">
                 <div class="form-group mx-2 mb-2 ">
     <label for="inputPassword2" class="sr-only" >Full Name</label>
-    <input type="text" class="form-control" placeholder="Full Name" name="fullName"/>
+    <input type="text" class="form-control" placeholder="Full Name" name="fullName" required/>
   </div>
   <div class="form-group mx-2 mb-2">
     <label for="staticEmail2" class="sr-only">Email</label>
-    <input type="text" class="form-control" placeholder="Email" name="email"/>
+    <input type="text" class="form-control" placeholder="Email" name="email" required/>
     <input type="hidden" name="lesson_id" value={lessonId}/>
   </div>
   <button type="submit" value="submit" class="btn btn-danger mx-2 mb-2">Class Sign Up</button>
