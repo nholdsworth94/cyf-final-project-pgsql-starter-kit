389c4de306b76e8705d936b86f560e3deb7c0f1c
Ali Haider <alihaiderx736@gmail.com>
2021-01-13 15:05:59 +0000

    -update lesson page info admin side

---
+++ client/src/AdminPage/LessonPage_Info.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const LessonPage_Info = ({lessonId,lesson,module,time,date,link}) => {
+  console.log(lessonId+"  landed on lesson page info")
+
+  return (
+    <div className="">
+      <h3 className="text-center"><strong>Westmidlands Class 1 </strong></h3>
+      <ul className="list-unstyled bg-light row my-2 mx-3">
+        <div className="col mx-2 my-2">
+        <li><strong>Module: </strong><span>{module}</span></li>
+        <li><strong>Date: </strong><span>{date}</span></li>
+        <li><strong>Time: </strong><span>{time}</span></li>
+        </div>
+        <div className="col mx-2 my-2">
+        <li><strong>Lesson: </strong><span>{lesson}</span></li>
+        <li><strong>Material: </strong><span><a href={link} target="_blank">{link}</a></span></li>
+     </div>
+      </ul>
+    </div>
+  );
+};
+
+export default LessonPage_Info;
