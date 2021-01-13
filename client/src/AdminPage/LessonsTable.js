7e29729ed41df1210178eba34932105d925ae0d5
Ali Haider <alihaiderx736@gmail.com>
2021-01-13 19:38:56 +0000

    -update api

--- client/src/AdminPage/LessonsTable.js
+++ client/src/AdminPage/LessonsTable.js
@@ -1,12 +1,13 @@
 import React from "react"
 import moment from 'moment';
 import { Link } from "react-router-dom";
+import {  useLocation } from "react-router-dom";
 
 
 const LessonsTable = ({el}) =>{
     
  const currentDate = moment(el.date).format("DD-MM-YY");
-
+  var deleteId;
 
    
    
@@ -24,7 +25,9 @@ date:currentDate,
 time:`${el.starttime}-${el.endtime}`,
 module:el.name,
 link:el.syllabus_link }  }}><button type="button" class="nav_Home rolesBTN"  >View Details</button></Link></td>
-<td><form method="DELETE" action={`https://cyf-finalproject-class-planner.herokuapp.com/api/signed_volunteers/${el.id}`}><button type="submit" value="submit" class="nav_Home rolesBTN"  >Delete Class</button></form></td> 
+<td>{deleteId=el.id}<form method="DELETE" action={`https://cyf-finalproject-class-planner.herokuapp.com/api/lesson/${deleteId}`}>
+  
+  <button type="submit" value="submit" class="nav_Home rolesBTN"  >Delete Class</button></form></td> 
     </tr>
         
 
