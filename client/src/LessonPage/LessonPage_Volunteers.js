32d9d522fb0b4ab54d8860c122bc7b5098e10b5e
Ali Haider <alihaiderx736@gmail.com>
2021-01-13 01:29:38 +0000

    - Update Lesson Page Voluntteers

--- client/src/LessonPage/LessonPage_Volunteers.js
+++ client/src/LessonPage/LessonPage_Volunteers.js
@@ -25,43 +25,21 @@ const [lessons, setLessons] = useState([]);
   return (
     <div>
       <h4 className="text-center"><strong>Volunteers at this Lesson:</strong></h4>
-      <table className="  container table-lg table-striped table-hover table-condensed  col-10">
-        <tbody className=" h-50 container  row d-flex justify-content-center overflow-auto">
-          <tr className="  row col-md-6 table-danger border border-danger rounded bg-danger">
+      <div className="mx-3 bg-light">
+              <div className="d-flex p-2 flex-wrap  mx-5 ">
+        {/**  <tr className="  row col-md-6 table-danger border border-danger rounded bg-danger">
             <td className=" col-md-6">Claire Bickley</td>
             <td className=" col-md-6">Regional Manager</td>
           </tr>
-          <tr className=" row col-md-6 table-primary border border-primary rounded bg-primary">
-            <td className="col-md-6">Emile Pafford-Wray</td>
-            <td className="col-md-6">Lead Teacher</td>
-          </tr>
-          <tr className="row col-md-6 table-success border border-success rounded bg-success">
-            <td className="col-md-6">Nadir</td>
-            <td className="col-md-6">Teaching Assistant</td>
-          </tr>
-          <tr className="row col-md-6 table-success border border-success rounded bg-success">
-            <td className="col-md-6">Nick Holdsworth</td>
-            <td className="col-md-6">Teaching Assistant</td>
-          </tr>
-          <tr className="row col-md-6 table-success border border-success rounded bg-success">
-            <td className="col-md-6">Wahab Rahman</td>
-            <td className="col-md-6">Teaching Assistant</td>
-          </tr>
-          <tr className="row col-md-6 table-warning border border-warning rounded bg-warning">
-            <td className="col-md-6">Edgar Endiulis</td>
-            <td className="col-md-6">Personal Development</td>
-          </tr>
-          <tr className="row col-md-6 table-info border border-info rounded bg-info">
-            <td className="col-md-6">Shukri</td>
-            <td className="col-md-6">Zoom Admin</td>
-          </tr>
-          <tr className="row col-md-6 table-secondary border border-secondary rounded bg-secondary">
-            <td className="col-md-6">Atanas Kozarev</td>
-            <td className="col-md-6">Host</td>
-          </tr>
-        </tbody>
-      </table>
+*/}         
+      {lessons.map(el => 
+            
+           <div className="d-inline-flex bg-light text-secondary p-1 justify-content-between col-4 mx-5 "><span className="text-dark"><strong>{el.role_id}</strong></span>{el.full_name}</div>
+           )}
+      </div>
     </div>
+    </div>
+
   );
 };
 
