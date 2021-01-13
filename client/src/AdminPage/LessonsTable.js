a490bb736199f14d324301d557af0943f37a482a
Ali Haider <alihaiderx736@gmail.com>
2021-01-13 18:00:39 +0000

    - update

--- client/src/AdminPage/LessonsTable.js
+++ client/src/AdminPage/LessonsTable.js
@@ -18,18 +18,13 @@ const LessonsTable = ({el}) =>{
       <td>{el.name}</td>
       <td>{el.week_number}</td>
       <td>{el.starttime+"-"+el.endtime}</td>
-      <td><Link to={{ state:{id:el.id,
+      <td><Link to={{ pathname: "/AdminLessonPage", state:{id:el.id,
 lesson:el.week_number,
 date:currentDate,
 time:`${el.starttime}-${el.endtime}`,
 module:el.name,
 link:el.syllabus_link }  }}><button type="button" class="nav_Home rolesBTN"  >View Details</button></Link></td>
-<td><Link to={{ pathname: "AdminPage", state:{id:el.id,
-lesson:el.week_number,
-date:currentDate,
-time:`${el.starttime}-${el.endtime}`,
-module:el.name,
-link:el.syllabus_link }  }}><form method="DELETE" action={`http://cyf-finalproject-class-planner.herokuapp.com/api/signed_volunteers/${el.id}`} ><button type="submit" value="submit" class="nav_Home rolesBTN"  >Delete Class</button></form></Link></td> 
+<td><form method="DELETE" action={`https://cyf-finalproject-class-planner.herokuapp.com/api/signed_volunteers/${el.id}`}><button type="submit" value="submit" class="nav_Home rolesBTN"  >Delete Class</button></form></td> 
     </tr>
         
 
