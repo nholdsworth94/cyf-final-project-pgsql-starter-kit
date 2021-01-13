de6ed02038598df63a30b5d89c09b39e9467222c
Ali Haider <alihaiderx736@gmail.com>
2021-01-13 18:06:52 +0000

    - update

--- client/src/UpcomingLessons/LessonsTable.js
+++ client/src/UpcomingLessons/LessonsTable.js
@@ -12,7 +12,7 @@ const LessonsTable = ({el}) =>{
    
 <tr>
       <td>{currentDate}</td>
-      <td>{el.name}</td>
+      <td>{el.module}</td>
       <td>{el.week_number}</td>
       <td>{el.starttime+"-"+el.endtime}</td>
       <td><Link to={{ pathname: "/LessonPage", state:{id:el.id,
