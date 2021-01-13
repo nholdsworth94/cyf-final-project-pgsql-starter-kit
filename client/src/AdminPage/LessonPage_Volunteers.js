64356cf6b46e53f65f3f10a1e45e76a99ca6ee56
Ali Haider <alihaiderx736@gmail.com>
2021-01-13 15:06:17 +0000

    -update lesson page volunteers.js

---
+++ client/src/AdminPage/LessonPage_Volunteers.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { useEffect, useState } from "react";
+
+
+
+const LessonPage_Volunteers = ({lessonId}) => {
+ console.log(lessonId+"  landed on lesson page volunteer component")
+const [lessons, setLessons] = useState([]);
+
+	useEffect(() => {
+    fetchData();
+	}, []);
+
+	const fetchData = async () => {
+		try {
+			const response = await fetch(`https://cyf-finalproject-class-planner.herokuapp.com/api/signed_volunteers/${lessonId}`);
+			const data = await response.json();
+			console.log(data.data);
+			setLessons(data.data);
+		} catch (error) {
+			console.log(error.message);
+		}
+  };
+
+  return (
+    <div>
+      <h4 className="text-center"><strong>Volunteers at this Lesson:</strong></h4>
+      <div className="mx-3 bg-light">
+              <div className="d-flex p-2 flex-wrap  mx-5 ">
+        {/**  <tr className="  row col-md-6 table-danger border border-danger rounded bg-danger">
+            <td className=" col-md-6">Claire Bickley</td>
+            <td className=" col-md-6">Regional Manager</td>
+          </tr>
+*/}         
+      {lessons.map(el => 
+            
+           <div className="d-inline-flex bg-light text-secondary p-1 justify-content-between col-4 mx-5 "><span className="text-dark"><strong>{el.role_id}</strong></span>{el.full_name}</div>
+           )}
+      </div>
+    </div>
+    </div>
+
+  );
+};
+
+export default LessonPage_Volunteers;
