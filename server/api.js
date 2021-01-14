9fcd584247cf14458dff2cf12c7964dd0ab5dfaa
Ali Haider <alihaiderx736@gmail.com>
2021-01-14 03:24:53 +0000

    -update

--- server/api.js
+++ server/api.js
@@ -174,8 +174,8 @@ router.post("/signed_volunteers", async (req, res) => {
  
 			const results = await pool.query(
 			"INSERT INTO signed_volunteers (full_name, email, lesson_id, role_id) VALUES ($1,$2,$3,$4)",[fullName,email,lesson_id,role]);
-		res.send(200);
 		
+
 	} catch (error) {
 		console.log(error.message);
 	}
