acf89db31fdab8332b2cf8b77d8dca9c794a66b6
Ali Haider <alihaiderx736@gmail.com>
2021-01-14 23:41:30 +0000

    update

--- client/src/UpcomingLessons/UpcomingLessons.js
+++ client/src/UpcomingLessons/UpcomingLessons.js
@@ -17,7 +17,7 @@ const UpcomingLessons =({RolesButtonHandler})=>{
 		try {
 			const response = await fetch('https://cyf-finalproject-class-planner.herokuapp.com/api/lesson');
 			const data = await response.json();
-			//console.log(data.data);
+			console.log(data.data);
 			setLessons(data.data);
 		} catch (error) {
 			console.log(error.message);
