d3d86dfd5cadc2072a945aa1fd292b8b778bb666
Ali Haider <alihaiderx736@gmail.com>
2021-01-14 00:45:51 +0000

    - update

--- server/api.js
+++ server/api.js
@@ -105,7 +105,7 @@ router.post("/register", async (req, res) => {
   let password = req.body.password;
   console.log(`\npassword: ${typeof password}`);
   console.log(`${password}`);
-		res.render('index');
+		res.render('index.html');
 		{/*res.status(200).json({
 			status: "success",
 			first_name: `${fname}`,
